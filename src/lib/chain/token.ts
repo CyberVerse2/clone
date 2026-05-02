5ac827b191a285e1bd30818307be53629eee3c06
Celestine <ejioforcelestine77@gmail.com>
2026-05-02 12:20:00 +0100

    Add clone identity migration

---
+++ src/lib/chain/token.ts
@@ -0,0 +1,16 @@
+/**
+ * Backward-compatible token domain exports.
+ *
+ * The implementation is now split into focused modules:
+ * - deploy
+ * - fees
+ * - market
+ * - pricing
+ * - transfers
+ */
+
+export * from './token/deploy';
+export * from './token/fees';
+export * from './token/market';
+export * from './token/pricing';
+export * from './token/transfers';
